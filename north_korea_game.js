d8df323aeb848f4954d7dbecf3a3444d922ff910
JulianNKQuiz <36675457+JulianNKQuiz@users.noreply.github.com>
2018-03-19 16:47:42 -0400

    Update north_korea_game.js

--- north_korea_game.js
+++ north_korea_game.js
@@ -1,5 +1,6 @@
 function startGame() {
     myGameArea.start();
+    myGamePiece = new component(2, 268, "red", 10, 120);
 }
 
 var myGameArea = {
@@ -9,5 +10,28 @@ var myGameArea = {
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
     }
+    
+clear : function() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+}
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y; 
+    this.update = function(){
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    
+function updateGameArea() {
+    myGameArea.clear();
+    myGamePiece.x += 1;
+    myGamePiece.update();
+}
+    
 }

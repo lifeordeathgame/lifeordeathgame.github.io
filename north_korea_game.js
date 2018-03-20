6b23141209916a9e46fc3bbd0c4ce928369fa8fd
JulianNKQuiz <36675457+JulianNKQuiz@users.noreply.github.com>
2018-03-19 22:35:51 -0400

    Update north_korea_game.js

--- north_korea_game.js
+++ north_korea_game.js
@@ -90,13 +90,15 @@ function updateGameArea() {
         myObstacles[i].update();
     }
 
-  /*  myGamePiece.speedX = 0;
+    myGamePiece.speedX = 0;
     myGamePiece.speedY = 0;    
-    if (myGameArea.key && myGameArea.key == 37) {myGamePiece.speedX = -1; }
-    if (myGameArea.key && myGameArea.key == 39) {myGamePiece.speedX = 1; }
-    if (myGameArea.key && myGameArea.key == 38) {myGamePiece.speedY = -1; }
-    if (myGameArea.key && myGameArea.key == 40) {myGamePiece.speedY = 1; }
-    myGamePiece.newPos();   */  
+    if (myGameArea.key && myGameArea.key == 37) {
+        myGamePiece.speedX = -1; 
+    }
+ //   if (myGameArea.key && myGameArea.key == 39) {myGamePiece.speedX = 1; }
+  //  if (myGameArea.key && myGameArea.key == 38) {myGamePiece.speedY = -1; }
+ //   if (myGameArea.key && myGameArea.key == 40) {myGamePiece.speedY = 1; }
+    myGamePiece.newPos();   
     myGamePiece.update(); 
 }
 

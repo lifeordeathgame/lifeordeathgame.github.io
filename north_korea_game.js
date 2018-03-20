52c20e3840c6ca84952471e9ee5086ef1c4ed765
JulianNKQuiz <36675457+JulianNKQuiz@users.noreply.github.com>
2018-03-20 16:37:57 -0400

    Update north_korea_game.js

--- north_korea_game.js
+++ north_korea_game.js
@@ -12,8 +12,8 @@ function startGame() {
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
+        this.canvas.width = 1280;
+        this.canvas.height = 720;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;

5dd9837a177f76e72f1fb799c31d3fd1591ed5eb
JulianNKQuiz <36675457+JulianNKQuiz@users.noreply.github.com>
2018-03-21 19:03:00 -0400

    Update north_korea_game.js

--- north_korea_game.js
+++ north_korea_game.js
@@ -3,7 +3,8 @@ var myObstacles = [];
 var myScore;
 
 function startGame() {
-    myGamePiece = new component(180, 90, "https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1280px-Flag_of_the_United_States.svg.png", 90, 655, "image");
+    var height = 90;
+    myGamePiece = new component(180, height, "https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1280px-Flag_of_the_United_States.svg.png", 90, 655, "image");
     myScore = new component("30px", "Consolas", "red", 50, 50, "text");
 
     myGameArea.start();

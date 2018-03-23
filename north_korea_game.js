ccd76f55f4d511155b969392a4ac31ea6db4d918
JulianNKQuiz <36675457+JulianNKQuiz@users.noreply.github.com>
2018-03-22 21:42:13 -0400

    Update north_korea_game.js

--- north_korea_game.js
+++ north_korea_game.js
@@ -105,18 +105,6 @@ function updateGameArea() {
         if (myGamePiece.crashWith(myObstacles[i])) {
 	    
             myGameArea.stop();
-	    swal({
-  		title: "Are you sure?",
-  		text: "You will not be able to recover this imaginary file!",
-  		type: "warning",
-  		showCancelButton: true,
-  		confirmButtonColor: "#DD6B55",
-  		confirmButtonText: "Yes, delete it!",
-  		closeOnConfirm: false
-	    },
-	    function(){
-  		swal("Deleted!", "Your imaginary file has been deleted.", "success");
-	    });
 	    window.location.reload();
             return;
         } 

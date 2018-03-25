0f961a7083409b5ca168cd11765c24d9c1a2e2b0
JulianNKQuiz <36675457+JulianNKQuiz@users.noreply.github.com>
2018-03-25 01:01:24 -0400

    Update north_korea_game.js

--- north_korea_game.js
+++ north_korea_game.js
@@ -6,6 +6,7 @@ function startGame() {
     myGamePiece = new component(180, 90, "https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1280px-Flag_of_the_United_States.svg.png", 90, 655, "image");
     myScore = new component("30px", "Consolas", "red", 50, 50, "text");
     mySound = new sound("hype.mp3");
+    myObstacles = [];
     myGameArea.start();
 }
 
@@ -105,11 +106,10 @@ function updateGameArea() {
     var x, y, z, score, moveup, movedown;
     for (i = 0; i < myObstacles.length; i += 1) {
         if (myGamePiece.crashWith(myObstacles[i])) {
-	    
-            myGameArea.stop();
-	    myGameArea.clear();
+	    myGameArea.stop();
 	    alert("You died!");
-	    startGame();
+    	    myGameArea.clear();
+            startGame();
             return;
         } 
     }

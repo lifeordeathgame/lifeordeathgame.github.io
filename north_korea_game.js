13a7fe72c6ca60d9806b26d05fb596c73780ee05
JulianNKQuiz <36675457+JulianNKQuiz@users.noreply.github.com>
2018-03-25 00:46:04 -0400

    Update north_korea_game.js

--- north_korea_game.js
+++ north_korea_game.js
@@ -6,6 +6,7 @@ function startGame() {
     myGamePiece = new component(180, 90, "https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1280px-Flag_of_the_United_States.svg.png", 90, 655, "image");
     myScore = new component("30px", "Consolas", "red", 50, 50, "text");
     mySound = new sound("National Anthem North Korea - 애국가.mp3");
+    mySound.play();
     myGameArea.start();
 }
 
@@ -223,8 +224,6 @@ function doneKey(e) {
     }
 }
 function changeBackground(){
-	alert("it working boi")
-	document.getElementById("canvas").style.background = "url(https://images.fineartamerica.com/images-medium-large-5/32-american-flag-les-cunliffe.jpg)"	
 	mySound.play();
 
 }

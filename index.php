7239cb5620dc973e9d87633a8d976081248bd113
JulianNKQuiz <36675457+JulianNKQuiz@users.noreply.github.com>
2018-04-03 20:35:58 -0400

    Update index.php

--- index.php
+++ index.php
@@ -26,5 +26,141 @@ $(document).ready(function(){
  
 });
 </script>
+<?
+session_start();
+ 
+function loginForm(){
+    echo'
+    <div id="loginform">
+    <form action="index.php" method="post">
+        <p>Please enter your name to continue:</p>
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name" />
+        <input type="submit" name="enter" id="enter" value="Enter" />
+    </form>
+    </div>
+    ';
+}
+ 
+if(isset($_POST['enter'])){
+    if($_POST['name'] != ""){
+        $_SESSION['name'] = stripslashes(htmlspecialchars($_POST['name']));
+    }
+    else{
+        echo '<span class="error">Please type in a name</span>';
+    }
+}
+?>
+<?php
+if(!isset($_SESSION['name'])){
+    loginForm();
+}
+else{
+?>
+<div id="wrapper">
+    <div id="menu">
+        <p class="welcome">Welcome, <b><?php echo $_SESSION['name']; ?></b></p>
+        <p class="logout"><a id="exit" href="#">Exit Chat</a></p>
+        <div style="clear:both"></div>
+    </div>    
+    <div id="chatbox"></div>
+     
+    <form name="message" action="">
+        <input name="usermsg" type="text" id="usermsg" size="63" />
+        <input name="submitmsg" type="submit"  id="submitmsg" value="Send" />
+    </form>
+</div>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
+<script type="text/javascript">
+// jQuery Document
+$(document).ready(function(){
+});
+</script>
+<?php
+}
+?>
+<p class="welcome">Welcome, <b><?php echo $_SESSION['name']; ?></b></p>
+<script type="text/javascript">
+// jQuery Document
+$(document).ready(function(){
+	//If user wants to end session
+	$("#exit").click(function(){
+		var exit = confirm("Are you sure you want to end the session?");
+		if(exit==true){window.location = 'index.php?logout=true';}		
+	});
+});
+</script>
+<?php
+ if(isset($_GET['logout'])){ 
+     
+    //Simple exit message
+    $fp = fopen("log.html", 'a');
+    fwrite($fp, "<div class='msgln'><i>User ". $_SESSION['name'] ." has left the chat session.</i><br></div>");
+    fclose($fp);
+     
+    session_destroy();
+    header("Location: index.php"); //Redirect the user
+}
+?>
+<script type="text/javascript"> 
+	//If user submits the form
+	$("#submitmsg").click(function(){	
+		var clientmsg = $("#usermsg").val();
+		$.post("post.php", {text: clientmsg});				
+		$("#usermsg").attr("value", "");
+		return false;
+	});
+ </script>
+ <?
+session_start();
+if(isset($_SESSION['name'])){
+    $text = $_POST['text'];
+     
+    $fp = fopen("log.html", 'a');
+    fwrite($fp, "<div class='msgln'>(".date("g:i A").") <b>".$_SESSION['name']."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
+    fclose($fp);
+}
+?>
+ <div id="chatbox"><?php
+if(file_exists("log.html") && filesize("log.html") > 0){
+    $handle = fopen("log.html", "r");
+    $contents = fread($handle, filesize("log.html"));
+    fclose($handle);
+     
+    echo $contents;
+}
+?></div>
+<script type="text/javascript"> 
+//Load the file containing the chat log
+	function loadLog(){		
+
+		$.ajax({
+			url: "log.html",
+			cache: false,
+			success: function(html){		
+				$("#chatbox").html(html); //Insert chat log into the #chatbox div				
+		  	},
+		});
+	}
+ 	//Load the file containing the chat log
+	function loadLog(){		
+		var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height before the request
+		$.ajax({
+			url: "log.html",
+			cache: false,
+			success: function(html){		
+				$("#chatbox").html(html); //Insert chat log into the #chatbox div	
+				
+				//Auto-scroll			
+				var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height after the request
+				if(newscrollHeight > oldscrollHeight){
+					$("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
+				}				
+		  	},
+		});
+	}
+ setInterval (loadLog, 2500);	//Reload file every 2500 ms or x ms if you wish to change the second parameter
+
+  </script>
 </body>
 </html>

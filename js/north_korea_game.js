319ab603abdf7b7bde16d197dc3d3c755b39a2ec
JulianNKQuiz <36675457+JulianNKQuiz@users.noreply.github.com>
2018-04-07 15:27:32 -0400

    Update north_korea_game.js

--- js/north_korea_game.js
+++ js/north_korea_game.js
@@ -8,7 +8,7 @@ function startGame() {
 , "image");
     myScore = new component("30px", "Consolas", "Red", 50, 50, "text");
     //myHighScore = new component("100px", "Consolas", "Black", window.innerWidth / 2 - 100, window.innerHeight / 2 - 100, "text");
-    mySound = new sound("hype.mp3");
+    mySound = new sound("Vivian and Nathan Duet.mp3");
     myObstacles = [];
     myGameArea.start();
 }
@@ -202,6 +202,7 @@ function sound(src) {
         this.sound.pause();
     }
 }
+mySound.play();
 document.onkeydown = checkKey;
 document.onkeyup = doneKey;
 

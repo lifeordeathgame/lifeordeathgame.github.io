b8cadb9c09a8caa139b9677d537f573a5c63ede7
JulianNKQuiz <36675457+JulianNKQuiz@users.noreply.github.com>
2018-05-04 21:41:35 -0400

    Update piano.js

--- js/piano.js
+++ js/piano.js
@@ -1,3 +1,4 @@
+
 alert("Press space, w, or up arrow to jump, and s, shift, or down arrow to crouch.")
 var myGamePiece;
 var myObstacles = [];
@@ -64,7 +65,8 @@ function component(width, height, color, x, y, type) {
         	ctx.fillStyle = color;
         	ctx.fillRect(this.x, this.y, this.width, this.height);
         }
-	this.newPos = function() {
+      }
+    this.newPos = function() {
     	this.sp += 0.005
     	if (this.gravitySpeed < 0){
     		this.gravitySpeed += this.gravity * 5;
@@ -77,15 +79,92 @@ function component(width, height, color, x, y, type) {
         this.y += this.speedY + this.gravitySpeed;
         this.hitBottom();
     }
-      }
-
     
+    this.hitBottom = function() {
+        var rockbottom = myGameArea.canvas.height - this.height;
+        
+        if (this.y > rockbottom) {
+            this.y = rockbottom;
+            
+        }
+    }
     
+    this.crashWith = function(otherobj) {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height);
+        var crash = true;
+        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
+            crash = false;
+        }
+        return crash;
+    }
 }
 
 function updateGameArea() {
     var x, y, z, score, ranfact
+    var ranfactnum = Math.floor((Math.random())* 5)+1;
+    if (ranfactnum == 1){
+        ranfact = "Did you know that over 5000 westerners a year travel to North Korea?"
+    }
+    else if (ranfactnum == 2) {
+	ranfact = "Did you know that Kim Il-Sung was handpicked by Russian oficials to rule North Korea?"   
+    }
+    else if (ranfactnum == 3) {
+	ranfact = "Did you know that Kim Jong-Un is a basketball fanatic?"
+    }
+    else if (ranfactnum == 4) {
+	ranfact = "Did you know that the DPRK is celebrating its 70th anniversary"   
+    }
+    else {
+	ranfact = "Did you know that Kim Jong-Un went to school in Switzerland?"
+    }
+    myGameArea.frameNo += 1;
+    score = (myGameArea.frameNo / 45);
+    score = Math.ceil(score);
+    for (i = 0; i < myObstacles.length; i += 1) {
+        if (myGamePiece.crashWith(myObstacles[i])) {
+            myGameArea.stop();
+	    myGameArea.clear();
+	    //alert("You died! Your score was " + score +". " + ranfact);
+	    startGame();
+            return;
+        } 
+    }
+    
     myGameArea.clear();
+    
+    z = 65;
+    if (myGameArea.frameNo == 1 || everyinterval(z)) {
+		var rannum = Math.floor((Math.random())* 10)+1;
+        	x = myGameArea.canvas.width;
+		if (rannum <= 5) {
+        	    //y = myGameArea.canvas.height - 105;
+		    myObstacles.push(new component(75, 75, "http://northkoreaworld.com/imgs/Kim.png", x, myGameArea.canvas.height - 75, "image"));
+		    myObstacles.push(new component(75, 75, "http://northkoreaworld.com/imgs/Kim.png", x, myGameArea.canvas.height - 310, "image"));
+
+		}
+		else if (rannum >= 6 && rannum <= 7 ){
+		    //y = myGameArea.canvas.height - 145;
+		    myObstacles.push(new component(75, 75, "http://northkoreaworld.com/imgs/Kim.png", x, myGameArea.canvas.height - 145, "image"));
+		    myObstacles.push(new component(75, 75, "http://northkoreaworld.com/imgs/Kim.png", x, myGameArea.canvas.height - 75, "image"));
+
+		}
+	        else if (rannum == 8 || rannum == 9 || rannum == 10) {
+		    //y = myGameArea.canvas.height - 145;
+		    myObstacles.push(new component(75, 75, "http://northkoreaworld.com/imgs/Kim.png", x, myGameArea.canvas.height - 145, "image"));
+		    myObstacles.push(new component(75, 75, "http://northkoreaworld.com/imgs/Kim.png", x, myGameArea.canvas.height - 220, "image"));
+
+		}
+	        else {
+		}
+    }
+    
     for (i = 0; i < myObstacles.length; i += 1) {
         myObstacles[i].x += - 25;
         myObstacles[i].update();
@@ -107,6 +186,9 @@ function updateGameArea() {
     //myHighScore.update();
     myGamePiece.newPos();    
     myGamePiece.update();
+}
+function restart() {
+    window.location.reload();
 }
 /*function sound(src) {
     this.sound = document.createElement("audio");
@@ -217,6 +299,35 @@ function doneKey(e) {
 
     }
 }
+function everyinterval(n) {
+    if ((myGameArea.frameNo / n) % 1 == 0) {return true;}
+    return false;
+}
+function accelerate(n) {
+	myGamePiece.gravitySpeed = n;
+}
+function moveup() {
+    myGamePiece.speedY = -1; 
+}
+
+function movedown() {
+    myGamePiece.speedY = 1; 
+}
+
+function moveleft() {
+    myGamePiece.speedX = -1; 
+}
+
+function moveright() {
+    myGamePiece.speedX = 1; 
+}
+
+function clearmove() {
+    myGamePiece.speedX = 0; 
+    myGamePiece.speedY = 0; 
+}
+
+
 
 
 
